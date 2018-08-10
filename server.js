beef313269ba9773a52c24043bf34cc261cd29f0
lvshenchao <rumble@bianjie.ai>
2018-08-10 17:35:45 +0800

    #IRISHOME-42 修改端口

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.get('*', isProd ? render : (req, res) => {
   readyPromise.then(() => render(req, res))
 })
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
 })
