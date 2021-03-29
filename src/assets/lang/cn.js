987091a43169cf21248a43914931d45868453d61
zhjibi742626083 <jinbiao@bianjie.ai>
2021-03-29 20:41:26 +0800

    update news and partners

--- src/assets/lang/cn.js
+++ src/assets/lang/cn.js
@@ -1,229 +1,238 @@
 export const message = {
-    "index": {
-        "logo": [
-            {
-                "src": '../public/banner_01.png',
-                "active": true
-            },
-            {
-                "src": '../public/index_logo_cn.png',
-                "active": false
-            },
-            {
-                "src": '../public/banner_01.png',
-                "active": true
-            },
-            {
-                "src": '../public/index_logo_cn.png',
-                "active": false
-            },
-        ],
-        "title": [
-            {"txt": "关于我们", "href": "/#about"},
-            {"txt": "研发技术", "href": "/#core"},
-            {"txt": "产品中心", "href": "/#product"},
-            {"txt": "公司动态", "href": "/#dynamic"},
-            {"txt": "合作伙伴", "href": "/#partner"},
-        ]
-    },
-    "about": {
-        "img": '../public/about.png',
-        "title": "关于我们",
-        "txt": [
-            '上海边界智能科技有限公司（Bianjie.AI）创立于2016年，是一家专注于分布式智能处理和区块链技术开发的高科技公司。自创立以来，边界智能以人工智能，大数据分析和区块链跨链技术创新为核心，开发了IRISnet跨链服务网络、分布式大数据分析交换网络（BEAN）、智能医药知识库等核心产品，实现以保护数据隐私、尊重数据拥有者权利为前提的加密智能分析服务，并与复星星康链等合作伙伴一起在医疗健康大数据领域开展系列创新实践。'+
-            '<br/><div style="text-indent: 34px">边界智能荣获第七届中国创新创业大赛成长组32强，中国首个面向以健康医疗大数据产业为核心的创新大赛 – CDHIC天使之星冠军等多个创新奖项。同时，边界智能也是全球著名跨链项目 COSMOS的核心中国开发者。</div>',
-        ]
-    },
-    "core": {
-        "title": "研发技术",
-        "secondaryTitle": "跨链服务",
-        "service": "支持异构网络间数字资产流动，以及数据和复杂计算的可信跨链调用。",
-        "technologyTitle": "分布式智能技术",
-        "technology": "运用人工智能与区块链技术，保护数据隐私、尊重数据所有权，并支持数据依靠智能算法安全分布式地使用。"
-    },
-    "product": {
-        "title": "产品中心",
-        "txt": [
-            {
-                "title": "IRISnet",
-                "bgImg": "../public/base_03.jpg",
-                "img": "../public/base_03_03.png",
-                "txt": "<a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>是边界智能作为核心开发团队与Cosmos/Tendermint的研发团队合作打造的区块链跨链网络，专注于为分布式应用提供基础设施和协议，也将是Cosmos网络生态中的第一个区域性枢纽(Hub)。" +
-                "<div><a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>将面向服务的基础设施融入到Cosmos网络中，支持对包括公链、联盟链以及现有传统商业系统的集成从而实现互联互通。通过对Cosmos网络的跨链协议进行创新扩展，<a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>允许数据及复杂计算跨异构网络被调用。" +
-                "</div><div>就像忠实地在人间和天堂传递信息的希腊彩虹女神Iris，<a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>的目标是成为链接数字经济和实体经济，支持构建复杂分布式商业应用的下一代公链。</div>"
-
-            },
-            {
-                "title": "BEAN",
-                "title1": "<span >B</span>lockchain <span>E</span>dge <span >A</span>nalytics <span >N</span>etwork",
-                "bgImg": "../public/base_01.jpg",
-                "img": "../public/base_01_01.png",
-                "txt": "<div>区块链智能信息边缘分析网络BEAN，是基于 <a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'>IRISnet</a> 技术为大数据分析打造的分布式智能信息分析和数据交换网络，通过AI和区块链技术释放数据价值，支持数据可信分析和交换。</div>" +
-                "<div>BEAN通过IRIS Hub和其他区块链网络交互，利用传统分布式人工智能研究中模型并行性和SOA设计模式，将区块链技术应用到分布式分析服务，实现与传统商业系统的链上链下通联，提供保护数据隐私、尊重数据拥有者权利的加密智能分析服务。" +
-                "边界智能基于BEAN开发出支持医疗数据分析的应用，为医疗数据提供链上基于语义技术智能抽取，让非结构化医疗数据在保护隐私的前提下进行结构化处理，使得链上分布式业务协作时数据搜索、交换及使用变成可能。</div>"
-            },
-        ]
-    },
-    "dynamic": {
-        "title": "公司动态",
-        "btnTxt:": "查看更多",
-        "txt": [
-            {
-                "year": '2020',
-                "day": "9",
-                "month": "Nov",
-                "title": "首批 BSN 跨链服务正式上线，已加入测试网供开发者体验",
-                "txt": "边界智能、分布科技与区块链服务网络 BSN 达成战略合作，于 2020 年 10 月 31 日，在 BSN 跨链通信枢纽（Interchain Communications Hub）中各自适配完成了 IRITA Hub 和 Poly Enterprise 这两种跨链服务并在 BSN 测试网上线。",
-                "href": "https://mp.weixin.qq.com/s/M-AW4M1dFFHxfy7L-mbRjA"
-            },
-            {
-                "year": '2020',
-                "day": "31",
-                "month": "Oct",
-                "title": "IRITA Hub 跨链服务枢纽成功上线 BSN 平台",
-                "txt": "边界智能打造的 IRITA Hub 跨链服务枢纽加入 BSN 跨链服务枢纽（ICH）并正式上线。IRITA Hub 是边界智能自主研发的 IRITA 联盟链产品线中专注跨链服务的产品，为打造 BSN 成为「全球性区块链互联网」提供重要的技术基础。",
-                "href": "https://mp.weixin.qq.com/s/nDQvQLaGdHqjBgTZnXEEXw"
-            },
-            {
-                "year": '2020',
-                "day": "26",
-                "month": "Oct",
-                "title": "边界智能荣获「2020 中国区块链技术创新典型企业」奖项",
-                "txt": "边界智能荣登《2020 中国区块链技术创新典型企业名录》。本次榜单由赛迪（青岛）区块链研究院在「链上未来·2020 区块链产业发展峰会」中颁布。",
-                "href": "https://mp.weixin.qq.com/s/H5tMcOKhpz64_h1oF3G1xw"
-            },
-            {
-                "year": '2020',
-                "day": "30",
-                "month": "Sep",
-                "title": "边界智能获授江西移动 BSN「金牌合作伙伴」，联合创始人曹恒于大会发表主题演讲",
-                "txt": "边界智能获授江西移动 BSN「金牌合作伙伴」，联合创始人曹恒发表主题为《IRITA Hub：新基建中的跨链服务枢纽——打造 BSN 支持隐私保护的跨链服务基础设施》的演讲。",
-                "href": "https://mp.weixin.qq.com/s/88013Tp5RMP2wdJHYk9vQw"
-            },
-            {
-                "year": '2020',
-                "day": "15",
-                "month": "Sep",
-                "title": "边界智能与中诚区块链研究院达成战略合作",
-                "txt": "边界智能与中诚区块链研究院建立了战略合作关系，将聚焦区块链“产、学、研”领域，将围绕央行数字货币（CBDC），证券化通证发行（STO），大数据，以及企业区块链应用等方面展开合作。",
-                "href": "https://mp.weixin.qq.com/s/t-VkHcTWrvmr0jNaO__uuQ"
-            },
-            {
-                "year": '2020',
-                "day": "15",
-                "month": "July",
-                "title": "边界智能创始人曹恒受邀出席 2020 长三角区块链技术与产业创新大会",
-                "txt": "边界智能创始人曹恒受邀出席 2020 长三角区块链技术与产业创新大会，在大会唯一的圆桌论坛上与业界大咖们围绕《区块链产业发展机遇和挑战》展开讨论。",
-                "href": "https://mp.weixin.qq.com/s/LkQBSep6ZIroNrXwaspz7A"
-            },
-          
-            {
-                "year": '2020',
-                "day": "5",
-                "month": "July",
-                "title": "边界智能加入区块链服务网络发展联盟",
-                "txt": "2020 年 7 月 5 日，边界智能正式成为 BSN 发展联盟的成员单位。此次公布的是自中国移动、中国联通、中国电信、人民网、百度、银联、民生银行等相继加入以来的第三批入盟成员单位。",
-                "href": "https://mp.weixin.qq.com/s/iVPY8m1_rfFJpsD9TEwx9A"
-            },
-            {
-                "year": '2020',
-                "day": "30",
-                "month": "June",
-                "title": "BSN、IRITA 和 Chainlink 达成共同建立 BSN 跨链和预言机服务的合作",
-                "txt": "IRISnet 将通过 IRITA 跨链服务枢纽支持 BSN 集成 Chainlink 预言机，共同为 BSN 的开发和发展做出重大贡献，推动区块链应用模式转型并在中国乃至世界范围更快增长与普及。",
-                "href": "https://mp.weixin.qq.com/s/GLKMtUprscz4JvPpE0OsnQ"
-            },
-            {
-                "year": '2020',
-                "day": "23",
-                "month": "June",
-                "title": "边界智能荣获 2020 长三角（上海）区块链应用创新大赛奖项",
-                "txt": "2020 年 6 月 21 日，由上海市科技创业中心、杨浦区科学技术委员会、上海区块链技术协会主办的 2020 长三角（上海）区块链应用创新大赛决赛圆满落下帷幕。边界智能历经海选和复赛，成功突围进入决赛。在 100 多家参赛的企业及团队项目中，边界的参赛方案《可跨链与保护数据隐私的供应链金融区块链平台》在决赛中排名前 6，于公共服务类的应用场景中排名最为靠前。",
-                "href": "https://mp.weixin.qq.com/s/6EcAqwMWq7QSjO90s12Yyw"
-            },
-            {
-                "year": '2020',
-                "day": "28",
-                "month": "May",
-                "title": "边界智能与南京壹证通一同推动区块链+可信身份的企业服务丨战略合作",
-                "txt": "近日，边界智能与南京壹证通达成了战略合作。双方本着互惠互利、诚信合作、共同发展的原则，将从区块链及安全身份认证领域发挥各自的优势，在身份服务、数字证书、时间戳、底层区块链技术等方面进行合作，大力推动区块链技术结合可信身份，一起推进创新技术落地应用的实现，创造商业价值去服务实体经济。",
-                "href": "https://mp.weixin.qq.com/s/ivRTLNK20HEtwGdzz6z6HQ"
-            },
-        ]
-    },
-    "partner": {
-        "title": "合作伙伴",
-        "img": [
-            [
-                {
-                    "img": "../public/partner/wx.png",
-                    "img_selected": "../public/partner/wx2.png",
-                    "href": "http://www.wxblockchain.com/"
-                },
-                {
-                    "img": "../public/partner/cosmos.png",
-                    "img_selected": "../public/partner/cosmos2.png",
-                    "href": "https://cosmos.network/"
-                },
-                {
-                    "img": "../public/partner/hdf.png",
-                    "img_selected": "../public/partner/hdf2.png",
-                    "href": "http://www.haodf.com/"
-                },
-                {
-                    "img": "../public/partner/nxyl.png",
-                    "img_selected": "../public/partner/nxyl2.png"
-                },
-            ],
-            [
-                
-                {
-                    "img": "../public/partner/bjdx.png",
-                    "img_selected": "../public/partner/bjdx2.png",
-                    "href": "http://www.bjmu.edu.cn/"
-                },
-                {
-                    "img": "../public/partner/xwl_gray.png",
-                    "img_selected": "../public/partner/xwl.png",
-                    "href": "http://www.tonghaocapital.com/"
-                },
-                {
-                    "img": "../public/partner/irisnet_gray.png",
-                    "img_selected": "../public/partner/irisnet.png",
-                    "href": "https://www.irisnet.org/"
-                },
-                {
-                    "img": "../public/partner/xkl_gray.png",
-                    "img_selected": "../public/partner/xkl.png",
-                    "href": "http://healthcloud.link/"
-                },
-            ],
-            [
-                {
-                    "img": "../public/partner/ptws_gray.png",
-                    "img_selected": "../public/partner/ptws.png",
-                    "href": "javascript:void(0)"
-                }
-            ]
-
-        ]
-    },
-    'irisnetInfo':{
-        "title":"IRIS Network",
-        "irisnetService":'面向服务的区块链跨链网络',
-        'irisnetTechnology':'新一代分布式商业生态的技术基础',
-        "irisnetWebsiteUrl":'www.irisnet.org'
-    },
-    "validator":{
-        "txt":[
-            "是基于权益证明（PoS）Tendermint共识算法",
-            "接入Cosmos区块链借助Hub将完成跨链价值转移",
-            "边界智能作为Cosmos的Hub验证节点",
-            "旨在维护Cosmos网络的安全性，促进生态发展！"
-        ],
-        'irisnetHref':"https://www.irisnet.org/",
-        "btnText":"委托 ATOM",
-        "btnHref":"/cosmos",
-        "headerValidatorBtnText":"委托 ATOM"
-    },
+	"index": {
+		"logo": [
+			{
+				"src": '../public/banner_01.png',
+				"active": true
+			},
+			{
+				"src": '../public/index_logo_cn.png',
+				"active": false
+			},
+			{
+				"src": '../public/banner_01.png',
+				"active": true
+			},
+			{
+				"src": '../public/index_logo_cn.png',
+				"active": false
+			},
+		],
+		"title": [
+			{"txt": "关于我们", "href": "/#about"},
+			{"txt": "研发技术", "href": "/#core"},
+			{"txt": "产品中心", "href": "/#product"},
+			{"txt": "公司动态", "href": "/#dynamic"},
+			{"txt": "合作伙伴", "href": "/#partner"},
+		]
+	},
+	"about": {
+		"img": '../public/about.png',
+		"title": "关于我们",
+		"txt": [
+			'上海边界智能科技有限公司（Bianjie.AI）创立于2016年，是一家专注于分布式智能处理和区块链技术开发的高科技公司。自创立以来，边界智能以人工智能，大数据分析和区块链跨链技术创新为核心，开发了IRISnet跨链服务网络、分布式大数据分析交换网络（BEAN）、智能医药知识库等核心产品，实现以保护数据隐私、尊重数据拥有者权利为前提的加密智能分析服务，并与复星星康链等合作伙伴一起在医疗健康大数据领域开展系列创新实践。' +
+			'<br/><div style="text-indent: 34px">边界智能荣获第七届中国创新创业大赛成长组32强，中国首个面向以健康医疗大数据产业为核心的创新大赛 – CDHIC天使之星冠军等多个创新奖项。同时，边界智能也是全球著名跨链项目 COSMOS的核心中国开发者。</div>',
+		]
+	},
+	"core": {
+		"title": "研发技术",
+		"secondaryTitle": "跨链服务",
+		"service": "支持异构网络间数字资产流动，以及数据和复杂计算的可信跨链调用。",
+		"technologyTitle": "分布式智能技术",
+		"technology": "运用人工智能与区块链技术，保护数据隐私、尊重数据所有权，并支持数据依靠智能算法安全分布式地使用。"
+	},
+	"product": {
+		"title": "产品中心",
+		"txt": [
+			{
+				"title": "IRISnet",
+				"bgImg": "../public/base_03.jpg",
+				"img": "../public/base_03_03.png",
+				"txt": "<a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>是边界智能作为核心开发团队与Cosmos/Tendermint的研发团队合作打造的区块链跨链网络，专注于为分布式应用提供基础设施和协议，也将是Cosmos网络生态中的第一个区域性枢纽(Hub)。" +
+					"<div><a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>将面向服务的基础设施融入到Cosmos网络中，支持对包括公链、联盟链以及现有传统商业系统的集成从而实现互联互通。通过对Cosmos网络的跨链协议进行创新扩展，<a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>允许数据及复杂计算跨异构网络被调用。" +
+					"</div><div>就像忠实地在人间和天堂传递信息的希腊彩虹女神Iris，<a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'> IRISnet </a>的目标是成为链接数字经济和实体经济，支持构建复杂分布式商业应用的下一代公链。</div>"
+				
+			},
+			{
+				"title": "BEAN",
+				"title1": "<span >B</span>lockchain <span>E</span>dge <span >A</span>nalytics <span >N</span>etwork",
+				"bgImg": "../public/base_01.jpg",
+				"img": "../public/base_01_01.png",
+				"txt": "<div>区块链智能信息边缘分析网络BEAN，是基于 <a style='color: #72ACE3' href='https://www.irisnet.org' target='_blank'>IRISnet</a> 技术为大数据分析打造的分布式智能信息分析和数据交换网络，通过AI和区块链技术释放数据价值，支持数据可信分析和交换。</div>" +
+					"<div>BEAN通过IRIS Hub和其他区块链网络交互，利用传统分布式人工智能研究中模型并行性和SOA设计模式，将区块链技术应用到分布式分析服务，实现与传统商业系统的链上链下通联，提供保护数据隐私、尊重数据拥有者权利的加密智能分析服务。" +
+					"边界智能基于BEAN开发出支持医疗数据分析的应用，为医疗数据提供链上基于语义技术智能抽取，让非结构化医疗数据在保护隐私的前提下进行结构化处理，使得链上分布式业务协作时数据搜索、交换及使用变成可能。</div>"
+			},
+		]
+	},
+	"dynamic": {
+		"title": "公司动态",
+		"btnTxt:": "查看更多",
+		"txt": [
+			{
+				"year": '2021',
+				"day": "27",
+				"month": "Mar",
+				"title": "边界智能成为中国中小企业协会产业区块链专委会常务理事单位",
+				"txt": "边界智能成为中国中小企业协会产业区块链专委会的 4 家常务理事单位之一，其联合创始人曹恒女士在第一次专委会会议上高票当选为常务理事会主席。",
+				"href": "https://mp.weixin.qq.com/s/q32jNkLaMnV8WGAiG6FcPw"
+			},
+			{
+				"year": '2021',
+				"day": "22",
+				"month": "Mar",
+				"title": "西井科技与边界智能达成战略合作，共同推动 AI 商业化应用的隐私保护及数据确权",
+				"txt": "西井科技与边界智能，将围绕人工智能应用过程中的数据的确权与隐私保护领域达成战略合作，致力于推动数字化服务生态体系的建立，将物联网人工智能与隐私计算以及区块链跨链技术相结合，有效解决数据采集、流通、应用等各个环节的隐私保护及数据确权问题。",
+				"href": "https://mp.weixin.qq.com/s/QhrF4peejZ18hQDslCVZgg"
+			},
+			{
+				"year": '2021',
+				"day": "20",
+				"month": "Feb",
+				"title": "联盟链、开放联盟链和公有链的区别和适用场景 | 金色财经采访实录",
+				"txt": "边界智能联合创始人曹恒接受金色财经采访，与红枣科技 CEO 何亦凡、FISCO BCOS 首席架构师张开翔共同探讨联盟链、开放联盟链和公有链的区别和应用场景。",
+				"href": "https://mp.weixin.qq.com/s/ueLHNLECoIo2eqyvYw8AnA"
+			},
+			{
+				"year": '2021',
+				"day": "31",
+				"month": "Jan",
+				"title": "首个 BSN 开放联盟链「文昌链」上线",
+				"txt": "BSN 第一个完成集成的开放联盟链「文昌链」已正式上线。「文昌链」是边界智能基于其自主可控联盟链产品 IRITA，改造了 IRIS Hub 和 Cosmos Hub 经过多年大规模实践验证过的前沿公有链底层技术，打造的支持多种智能合约的开放联盟链。",
+				"href": "https://mp.weixin.qq.com/s/F0QEBBG1uDVDWAEQDXMPBg"
+			},
+			{
+				"year": '2021',
+				"day": "4",
+				"month": "Jan",
+				"title": "边界智能获 ISO9001 及 ISO27001 国际认证",
+				"txt": "边界智能完成了内部标准化管理体系建设的又一个里程碑：获得了 ISO9001 及 ISO27001 两项认证，这标志着边界智能在质量管理和信息安全管理方面达到了国际标准规范要求。",
+				"href": "https://mp.weixin.qq.com/s/dtRYOw2_LPJXEg_8ZdpN3g"
+			},
+			{
+				"year": '2021',
+				"day": "1",
+				"month": "Jan",
+				"title": "边界智能 2020 回顾与新年展望",
+				"txt": "边界智能在 2020 年里自研发布了成熟创新的区块链产品，专注于前沿技术研发，不断提升针对行业及业务场景深度融合与咨询定制能力，还与国内外行业领袖围绕生态发展达成深度合作。",
+				"href": "https://mp.weixin.qq.com/s/l3S0IPjdelY2pBGgysCViQ"
+			},
+			
+			{
+				"year": '2020',
+				"day": "5",
+				"month": "July",
+				"title": "边界智能加入区块链服务网络发展联盟",
+				"txt": "2020 年 7 月 5 日，边界智能正式成为 BSN 发展联盟的成员单位。此次公布的是自中国移动、中国联通、中国电信、人民网、百度、银联、民生银行等相继加入以来的第三批入盟成员单位。",
+				"href": "https://mp.weixin.qq.com/s/iVPY8m1_rfFJpsD9TEwx9A"
+			},
+			{
+				"year": '2020',
+				"day": "16",
+				"month": "Dec",
+				"title": "边界智能入选 2020 上海最具投资潜力 50 佳创业企业",
+				"txt": "边界智能凭借「支持跨链及大数据隐私保护的分布式商业系统 IRITA」顺利突出重围，成功获评「2020 上海最具投资潜力 50 佳创业企业」，是 50 佳获奖企业中为数不多的专注于区块链与大数据技术服务的高新技术企业。",
+				"href": "https://mp.weixin.qq.com/s/j6qgXPJJcbKvTxZHYLYsTg"
+			},
+			
+			{
+				"year": '2020',
+				"day": "14",
+				"month": "Dec",
+				"title": "万字庖解区块链跨链技术 | 金色财经采访实录",
+				"txt": " 边界智能联合创始人曹恒接受金色财经采访，与红枣科技 CEO 何亦凡、分布科技创始人达鸿飞共同探讨区块链跨链技术与服务。",
+				"href": "https://mp.weixin.qq.com/s/5FPf1q5QFFLC3BYuJzjC4Q"
+			},
+			{
+				"year": '2020',
+				"day": "4",
+				"month": "Dec",
+				"title": "边界智能成为 IEEE 标准协会高级会员，参与区块链标准制定",
+				"txt": "边界智能正式成为 IEEE 标准协会高级会员，参与区块链跨链互操作等相关领域的标准制定。",
+				"href": "https://mp.weixin.qq.com/s/2aK7L4qCswbSDwTdFK-PaA"
+			},
+			{
+				"year": '2020',
+				"day": "9",
+				"month": "Nov",
+				"title": "首批 BSN 跨链服务正式上线，已加入测试网供开发者体验",
+				"txt": "边界智能、分布科技与区块链服务网络 BSN 达成战略合作，于 2020 年 10 月 31 日，在 BSN 跨链通信枢纽（Interchain Communications Hub）中各自适配完成了 IRITA Hub 和 Poly Enterprise 这两种跨链服务并在 BSN 测试网上线。",
+				"href": "https://mp.weixin.qq.com/s/M-AW4M1dFFHxfy7L-mbRjA"
+			},
+		]
+	},
+	"partner": {
+		"title": "合作伙伴",
+		"img": [
+			[
+				{
+					"img": "../public/partner/wx.png",
+					"img_selected": "../public/partner/wx2.png",
+					"href": "http://www.wxblockchain.com/"
+				},
+				{
+					"img": "../public/partner/cosmos.png",
+					"img_selected": "../public/partner/cosmos2.png",
+					"href": "https://cosmos.network/"
+				},
+				{
+					"img": "../public/partner/hdf.png",
+					"img_selected": "../public/partner/hdf2.png",
+					"href": "http://www.haodf.com/"
+				},
+				{
+					"img": "../public/partner/nxyl.png",
+					"img_selected": "../public/partner/nxyl2.png"
+				},
+			],
+			[
+				
+				{
+					"img": "../public/partner/bjdx.png",
+					"img_selected": "../public/partner/bjdx2.png",
+					"href": "http://www.bjmu.edu.cn/"
+				},
+				{
+					"img": "../public/partner/xwl_gray.png",
+					"img_selected": "../public/partner/xwl.png",
+					"href": "http://www.tonghaocapital.com/"
+				},
+				{
+					"img": "../public/partner/irisnet_gray.png",
+					"img_selected": "../public/partner/irisnet.png",
+					"href": "https://www.irisnet.org/"
+				},
+				{
+					"img": "../public/partner/xkl_gray.png",
+					"img_selected": "../public/partner/xkl.png",
+					"href": "http://healthcloud.link/"
+				},
+			],
+			[
+				{
+					"img": "../public/partner/ptws_gray.png",
+					"img_selected": "../public/partner/ptws.png",
+					"href": "javascript:void(0)"
+				}
+			]
+		
+		]
+	},
+	'irisnetInfo': {
+		"title": "IRIS Network",
+		"irisnetService": '面向服务的区块链跨链网络',
+		'irisnetTechnology': '新一代分布式商业生态的技术基础',
+		"irisnetWebsiteUrl": 'www.irisnet.org'
+	},
+	"validator": {
+		"txt": [
+			"是基于权益证明（PoS）Tendermint共识算法",
+			"接入Cosmos区块链借助Hub将完成跨链价值转移",
+			"边界智能作为Cosmos的Hub验证节点",
+			"旨在维护Cosmos网络的安全性，促进生态发展！"
+		],
+		'irisnetHref': "https://www.irisnet.org/",
+		"btnText": "委托 ATOM",
+		"btnHref": "/cosmos",
+		"headerValidatorBtnText": "委托 ATOM"
+	},
 }
