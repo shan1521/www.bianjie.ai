54b8c5bc1a7dd4f4e32bde8ec9471eee6e0fbabc
Jinbiao Zhang <zhangjinbiao9693@gmail.com>
2021-07-07 22:05:18 +0800

    Update ProductionIrita.vue

--- src/components/ProductionIrita.vue
+++ src/components/ProductionIrita.vue
@@ -76,7 +76,7 @@
 					{
 						src: require("../assets/irita/tx.png"),
 						title:'数字资产建模与交易',
-						section:'支持对实体资产数据多元结构的灵活建模非常适合实现各领域的资产数字化'
+						section:'支持对实体资产数据多元结构的灵活建模，非常适合实现各领域的资产数字化'
 					},
 					{
 						src: require("../assets/irita/data.png"),
