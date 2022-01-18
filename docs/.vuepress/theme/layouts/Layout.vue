e0cd3fbd52559af0c73b09a4867fdde10bf30c34
shanshan <shan@bianjie.ai>
2022-01-18 14:07:50 +0800

    clear files

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -2,8 +2,6 @@
     <div
         class="theme-container"
         :class="pageClasses"
-        @touchstart="onTouchStart"
-        @touchend="onTouchEnd"
     >
         <ClientOnly>
             <Navigation></Navigation>
@@ -33,10 +31,6 @@
 </template>
 
 <script>
-import Home from "@theme/components/Home.vue";
-import Navbar from "@theme/components/Navbar.vue";
-import Page from "@theme/components/Page.vue";
-import Sidebar from "@theme/components/Sidebar.vue";
 import Navigation from "@theme/components/Navigation.vue";
 import NewHome from "@theme/components/Home/NewHome.vue";
 import IritaHub from "@theme/components/Product/IritaHub.vue";
@@ -88,38 +82,6 @@ export default {
                 return '$page.frontmatter.isDataCollection';
             }
         },
-        shouldShowNavbar() {
-            const { themeConfig } = this.$site;
-            const { frontmatter } = this.$page;
-            if (frontmatter.navbar === false || themeConfig.navbar === false) {
-                return false;
-            }
-            return (
-                this.$title ||
-                themeConfig.logo ||
-                themeConfig.repo ||
-                themeConfig.nav ||
-                this.$themeLocaleConfig.nav
-            );
-        },
-
-        shouldShowSidebar() {
-            const { frontmatter } = this.$page;
-            return (
-                !frontmatter.home &&
-                frontmatter.sidebar !== false &&
-                this.sidebarItems.length
-            );
-        },
-
-        sidebarItems() {
-            return resolveSidebarItems(
-                this.$page,
-                this.$page.regularPath,
-                this.$site,
-                this.$localePath
-            );
-        },
 
         pageClasses() {
             const userPageClass = this.$page.frontmatter.pageClass;
@@ -134,31 +96,6 @@ export default {
         },
     },
     methods: {
-        toggleSidebar(to) {
-            this.isSidebarOpen =
-                typeof to === "boolean" ? to : !this.isSidebarOpen;
-            this.$emit("toggle-sidebar", this.isSidebarOpen);
-        },
-
-        // side swipe
-        onTouchStart(e) {
-            this.touchStart = {
-                x: e.changedTouches[0].clientX,
-                y: e.changedTouches[0].clientY,
-            };
-        },
-
-        onTouchEnd(e) {
-            const dx = e.changedTouches[0].clientX - this.touchStart.x;
-            const dy = e.changedTouches[0].clientY - this.touchStart.y;
-            if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-                if (dx > 0 && this.touchStart.x <= 80) {
-                    this.toggleSidebar(true);
-                } else {
-                    this.toggleSidebar(false);
-                }
-            }
-        },
     },
     mounted() {
         this.$router.afterEach(() => {
@@ -179,10 +116,6 @@ export default {
         },
     },
     components: {
-        Home,
-        Page,
-        Sidebar,
-        Navbar,
         Navigation,
         NewHome,
         IritaHub,
